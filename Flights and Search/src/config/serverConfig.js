9d17673034a72c64545e9f8924de2e5f0c1c1110
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-03-06 23:07:31 +0530

    setup-basic Bodyparser

--- Flights and Search/src/config/serverConfig.js
+++ Flights and Search/src/config/serverConfig.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
 module.exports = {
     PORT:process.env.PORT
 }
--- Flights and Search/src/index.js
+++ Flights and Search/src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+//require('dotenv').config()
+const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig')
+const {PORT} = require('./config/serverConfig');
+
+
 
 const setupAndStart= async() =>
 {
     //create the exress object
     const app = express();
-    const PORT = 3000;
 
-    app.listen(3000, () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    
+
+    app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
+      
     })
 }
 setupAndStart();
